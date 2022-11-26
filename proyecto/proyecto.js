564241eef6f5ad61325b0b2749d42c9074d99399
juanCsacasa <110007393+juanCsacasa@users.noreply.github.com>
2022-11-26 01:51:50 -0600

    Add files via upload

---
+++ proyecto/proyecto.js
@@ -0,0 +1,98 @@
+//hay dos porque el 4 me salia que es un numero primo y no lo es asi que hice varias pruebas porque me parecia muy raro y busacando me di cuenta de que era tipo como una excepcion tal vez.
+/*function primo(numero) {
+  //console.log(numero);
+  for (let i = 2; i < numero / 2; i++) {
+    if (numero % i === 0 && numero / i !== 1) {
+      return "no es primo";
+    }
+  }
+  return "es primo"
+}
+console.log(primo(4));
+
+function primo2(numero) {
+  let primo = true;
+  for (let i = 2; i < numero / 2; i++) {
+    if (numero % i === 0) {
+      primo = false;
+    }
+  }
+  if (primo) {
+    return  numero + " es un numero primo";
+  } else {
+    return  numero + " no es un numero primo";
+  }
+}
+console.log(primo2(4));*/
+
+/*function ecuacion_cuadratica(params) {
+  
+}*/
+
+/*function unoAlCien(numero) {
+  for (let i = 1; i <= numero; i++) {
+    console.log(i)
+  }
+}
+unoAlCien(100)*/
+
+/*function unoAlCien(numero) {
+  for (let i = 1; i <= numero; i++) {
+    if (i % 2 ===0) {
+    console.log(i)
+    }
+  }
+}
+unoAlCien(100);*/
+
+/*function unoAlCien(numero) {
+  for (let i = 1; i <= numero; i++) {
+    if (i >= 1 && i <= 10) {
+      console.log(i + " :de los numeros del 1 al 10");
+    }
+    if(i >= 11 && i <= 20) {
+      console.log(i + " :de los numeros del 11 al 20");
+    }
+    if(i >= 21 && i <= 30) {
+      console.log(i + " :de los numeros del 21 al 30");
+    }
+    if(i >= 31 && i <= 40) {
+      console.log(i + " :de los numeros del 31 al 40");
+    }
+    if(i >= 41 && i <= 50) {
+      console.log(i + " :de los numeros del 41 al 50");
+    }
+    if(i >= 51 && i <= 60) {
+      console.log(i + " :de los numeros del 51 al 60");
+    }
+    if(i >= 61 && i <= 70) {
+      console.log(i + " :de los numeros del 61 al 70");
+    }
+    if(i >= 71 && i <= 80) {
+      console.log(i + " :de los numeros del 71 al 80");
+    }
+    if(i >= 81 && i <= 90) {
+      console.log(i + " :de los numeros del 81 al 90");
+    }
+    if(i >= 91 && i <= 100) {
+      console.trace(i + " :de los numeros del 91 al 100");
+    }
+  }
+}
+unoAlCien(100);*/
+
+//intento de hcaer el 5
+/*
+let lista = [];
+function unoAlCien() {
+  let contador = 0;
+  for (let i = 1; i <= 100; i++) {
+    contador +=
+    contador.push(contador)
+    //console.log(i);
+    if (contador == 10) {
+        lista.push(i)
+    }
+  }
+  console.log(lista)
+}*/
